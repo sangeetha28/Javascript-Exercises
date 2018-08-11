5ac2dc5a0129fc5faf79b31831ba40bda8395cb2
Sangeetha Rajasekaran <sangeetharajasekaran@notonthehighstreet.com>
2018-08-11 17:08:23 +0100

    promise.all

--- Promises/promiseall.js
+++ Promises/promiseall.js
@@ -1,4 +1,3 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 /**The Promise.all(iterable) method returns a single /**Promise that resolves when all of the promises in the iterable argument have resolved or when the iterable argument contains
  no promises.**/
 
@@ -49,4 +48,5 @@ const asynchronousFunction = (time) => {
 }
 
 PromiseAll([asynchronousFunction(2000),asynchronousFunction(1000),asynchronousFunction(1000)]).then(res => console.log('result is',res))
-},{}]},{},[1]);
+
+// [2000,1000,1000]
