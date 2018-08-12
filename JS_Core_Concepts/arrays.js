2651b56b09ae802c954c90923c8c2175ec9e4c06
Sangeetha Rajasekaran <sangeetharajasekaran@notonthehighstreet.com>
2018-08-12 16:26:46 +0100

    js notes

---
+++ JS_Core_Concepts/arrays.js
@@ -0,0 +1,16 @@
+/* Javascript Arrays can hold the Collection of Anything */ 
+
+var arr = [
+1,
+false,
+{
+    name: 'Rosa',
+    address: 'Richmond'
+},
+function(){
+  console.log('This is not a function Statement, But a Function expression!')
+},
+"If you want, work for it! Simple as that..."
+];
+
+console.log(arr);
---
+++ JS_Core_Concepts/this_in_inner_functions_inside_object.js
@@ -0,0 +1,23 @@
+'This' keyword in inner function inside the Object refers to the Global Object that is Windows.
+
+Usually 'This' inside the Object method will refer to the Object itself, but nested function inside the object will refer to the Global
+window Object
+
+var obj =
+{
+firstName: 'Sangeetha',
+lastName: 'R',
+text: 'You are Special',
+greet: function(){
+   console.log('Hello',this.firstName+this.text);
+
+   language: function() {
+      this.text = 'You are Simply Fabulous!'; // Remember this text will be appened to the Window Object and will not override the current object text
+   }
+}
+}
+
+
+  var greety = obj.greet();
+  greety.language();
+  console.log(obj.text);
